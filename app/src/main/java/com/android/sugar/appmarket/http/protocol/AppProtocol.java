aa1ac3b1cd981bf31c09979b9c0cbca4c0e76857
Sugar.T <SugarT@SugarTdeMacBook-Pro.local>
2016-07-25 10:48:04 +0800

    add protocol base

---
+++ app/src/main/java/com/android/sugar/appmarket/http/protocol/AppProtocol.java
@@ -0,0 +1,10 @@
+package com.android.sugar.appmarket.http.protocol;
+
+/**
+ * @ClassName: AppProtocol
+ * @Description:
+ * @author: SugarT
+ * @date: 16/7/25 上午10:45
+ */
+public class AppProtocol {
+}
