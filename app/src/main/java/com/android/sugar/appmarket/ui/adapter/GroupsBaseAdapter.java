4959dc2d11122bfdce82ae01392a4cd153a6bbc1
Sugar.T <SugarT@SugarTdeMacBook-Pro.local>
2016-07-25 11:29:35 +0800

    fix bug on DetailActivity

---
+++ app/src/main/java/com/android/sugar/appmarket/ui/adapter/GroupsBaseAdapter.java
@@ -0,0 +1,10 @@
+package com.android.sugar.appmarket.ui.adapter;
+
+/**
+ * @ClassName: GroupsBaseAdapter
+ * @Description:
+ * @author: SugarT
+ * @date: 16/7/25 上午11:28
+ */
+public class GroupsBaseAdapter {
+}
---
+++ app/src/main/java/com/android/sugar/appmarket/ui/adapter/ListBaseAdapter.java
@@ -0,0 +1,10 @@
+package com.android.sugar.appmarket.ui.adapter;
+
+/**
+ * @ClassName: ListBaseAdapter
+ * @Description:
+ * @author: SugarT
+ * @date: 16/7/25 上午11:28
+ */
+public class ListBaseAdapter {
+}
