f7673918209a06baac08df3c182b3c3bbb598957
Sugar.T <SugarT@SugarTdeMacBook-Pro.local>
2016-07-25 11:31:51 +0800

    add adapters

--- app/src/main/java/com/android/sugar/appmarket/ui/adapter/ListBaseAdapter.java
+++ app/src/main/java/com/android/sugar/appmarket/ui/adapter/ListBaseAdapter.java
@@ -1,10 +1,80 @@
 package com.android.sugar.appmarket.ui.adapter;
 
+import android.content.Intent;
+import android.widget.AbsListView;
+
+import com.android.sugar.appmarket.bean.AppInfo;
+import com.android.sugar.appmarket.bean.DownloadInfo;
+import com.android.sugar.appmarket.manager.DownloadManager;
+import com.android.sugar.appmarket.ui.activity.DetailActivity;
+import com.android.sugar.appmarket.ui.holder.BaseHolder;
+import com.android.sugar.appmarket.ui.holder.ListBaseHolder;
+import com.android.sugar.appmarket.utils.UIUtils;
+
+import java.util.List;
+
 /**
  * @ClassName: ListBaseAdapter
  * @Description:
  * @author: SugarT
  * @date: 16/7/25 上午11:28
  */
-public class ListBaseAdapter {
+public abstract class ListBaseAdapter extends DefaultAdapter<AppInfo> implements DownloadManager.DownloadObserver {
+
+    public ListBaseAdapter(AbsListView listView, List<AppInfo> datas) {
+        super(listView, datas);
+    }
+
+    @Override
+    protected BaseHolder getHolder() {
+        return new ListBaseHolder();
+    }
+
+    public void startObserver() {
+        DownloadManager.getInstance().registerObserver(this);
+    }
+
+    public void stopObserver() {
+        DownloadManager.getInstance().unRegisterObserver(this);
+    }
+
+    @Override
+    public void onDownloadStateChanged(final DownloadInfo info) {
+        refreshHolder(info);
+    }
+
+    @Override
+    public void onDownloadProgressed(DownloadInfo info) {
+        refreshHolder(info);
+    }
+
+    private void refreshHolder(final DownloadInfo info) {
+        List<BaseHolder> displayedHolders = getDisplayedHolders();
+        for (int i = 0; i < displayedHolders.size(); i++) {
+            BaseHolder baseHolder = displayedHolders.get(i);
+            if (baseHolder instanceof ListBaseHolder) {
+                final ListBaseHolder holder = (ListBaseHolder) baseHolder;
+                AppInfo appInfo = holder.getData();
+                if (appInfo.getId() == info.getId()) {
+                    UIUtils.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            holder.refreshState(info.getDownloadState(), info.getProgress());
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onItemClickInner(int position) {
+        List<AppInfo> data = getData();
+        if (position < data.size()) {
+            Intent intent = new Intent(UIUtils.getContext(), DetailActivity.class);
+            AppInfo info = data.get(position);
+            intent.putExtra(DetailActivity.PACKAGENAME, info.getPackageName());
+            UIUtils.startActivity(intent);
+        }
+    }
 }
